a2ccca6fd836b3688cac210f3fcd8ec5813e4590
Mert Korkmaz <krkmaz2004@gmail.com>
2025-12-09 18:46:46 +0300

    migration ve update-databse yapıldı

--- ECommerce.Data/Context/AppDbContext.cs
+++ ECommerce.Data/Context/AppDbContext.cs
@@ -10,10 +10,11 @@ namespace ECommerce.Data.Context
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
+       
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
