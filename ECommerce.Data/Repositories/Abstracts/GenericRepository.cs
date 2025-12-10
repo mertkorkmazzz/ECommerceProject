5a3c44e70de51a46063f249908b33198c36de969
Mert Korkmaz <krkmaz2004@gmail.com>
2025-12-10 20:43:28 +0300

    crud işlemler yapıldı

---
+++ ECommerce.Data/Repositories/Abstracts/GenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Data.Repositories.Abstracts
+{
+    public class GenericRepository  :IGenericRepository
+    {
+    }
+}
---
+++ ECommerce.Data/Repositories/Abstracts/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Data.Repositories.Abstracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int ıd);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T ıd);
+
+    }
+}
