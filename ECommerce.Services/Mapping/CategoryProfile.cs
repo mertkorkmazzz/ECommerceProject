c5b8aece0f7c932ad827ed1dc279f5750b26b4aa
Mert Korkmaz <krkmaz2004@gmail.com>
2025-12-11 23:57:32 +0300

    dto ve automappe işlemleri yapıldı

---
+++ ECommerce.Services/Mapping/CategoryProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ECommerce.Entities.Entities;
+using ECommerce.Services.DTOs.CategoryDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Services.Mapping
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryListDto>();
+            CreateMap<Category, CategoryUpdateDto>();
+            CreateMap<CategoryCreateDto, Category>();
+            CreateMap<CategoryUpdateDto, Category>();
+        }
+    }
+}
