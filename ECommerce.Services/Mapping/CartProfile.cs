773dafdfe7f69ea87c6407d81b1b29a5c6f3b14b
Mert Korkmaz <krkmaz2004@gmail.com>
2025-12-14 14:01:30 +0300

    Dto ve AutoMapper yapıldı

--- ECommerce.Services/Mapping/CartProfile.cs
+++ ECommerce.Services/Mapping/CartProfile.cs
@@ -28,3 +28,36 @@ namespace ECommerce.Services.Mapping
         }
     }
 }
+//AUTOMAPPER nedir
+// AutoMapper, .NET uygulamalarında nesneler arasında veri dönüşümünü kolaylaştıran popüler bir kütüphanedir.
+// Özellikle veri transfer nesneleri (DTO'lar) ile varlık modelleri (Entity'ler) arasında veri kopyalamak için kullanılır.
+// AutoMapper, kaynak nesnenin özelliklerini hedef nesnenin karşılık gelen özelliklerine otomatik olarak eşler ve böylece manuel olarak veri kopyalama ihtiyacını ortadan kaldırır.
+
+
+
+//Profile nedir
+// Profile, AutoMapper kütüphanesinde kullanılan bir sınıftır ve nesneler arasındaki eşleme (mapping) yapılandırmalarını tanımlamak için kullanılır.
+// yani mapping kurrallarını tutan bir yerdir
+//mapping kurllarını saklamak ve auto mapper a bildirmek için kullanılır
+
+
+//createMap nedir
+//Hangi nesnenin hangi nesneye nasıl gideceğini tanımlayan fonksiyon.
+
+
+//forMember nedir
+// Belirli bir üyenin (property) nasıl eşleneceğini yapılandırmak için kullanılır.
+// Örneğin, bir kaynaktaki "FirstName" özelliğini hedefteki "Name" özelliğine eşlemek için kullanılabilir.
+// .ForMember(x => x.PropertyName, opt => opt.MapFrom(src => src.OtherPropertyName))
+
+
+//mapFrom nedir
+// Kaynak nesneden hedef nesneye veri kopyalama işlemini tanımlamak için kullanılır.
+// Örneğin, bir kaynaktaki "FirstName" özelliğini hedefteki "Name" özelliğine eşlemek için kullanılabilir.
+// .ForMember(x => x.PropertyName, opt => opt.MapFrom(src => src.OtherPropertyName))
+
+
+//forMember ile mapFrom arasındaki fark nedir
+// forMember, hedef nesnedeki belirli bir üyenin (property) nasıl eşleneceğini yapılandırmak için kullanılır.
+// mapFrom ise, forMember içinde kullanılarak, kaynak nesneden hedef nesneye veri kopyalama işlemini tanımlamak için kullanılır.
+// Yani, forMember hedefteki üyeyi belirtirken, mapFrom kaynaktaki verinin nereden geleceğini belirtir.
